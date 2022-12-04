4edfec23973b27010d7f19291531ac79e363cf14
Marcus Aidley <marcus.aidley@globalrelay.net>
2022-12-04 09:34:25 +0000

    Day 4 tidy up

--- src/main/java/com/marcus/advent/twentytwo/Day04.java
+++ src/main/java/com/marcus/advent/twentytwo/Day04.java
@@ -4,33 +4,40 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.function.Predicate;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class Day04 {
     public long part1() throws IOException {
         try (Stream<String> stream = Files.lines(Paths.get("./src/main/resources/day-04.txt"))) {
-            return stream
-                    .map(s -> s.split(","))
-                    .map(a -> String.join("-", a).split("-"))
-                    .map(a -> Arrays.stream(a).mapToInt(Integer::parseInt))
-                    .map(IntStream::toArray)
-                    .filter(a -> (a[0] <= a[2] && a[1] >= a[3]) ||  (a[0] >= a[2] && a[1] <= a[3]))
-                    .count();
+            return getResult(stream, this::subSetFilter);
         }
-        
     }
 
     public long part2() throws IOException {
         try (Stream<String> stream = Files.lines(Paths.get("./src/main/resources/day-04.txt"))) {
-            return stream
-                    .map(s -> s.split(","))
-                    .map(a -> String.join("-", a).split("-"))
-                    .map(a -> Arrays.stream(a).mapToInt(Integer::parseInt))
-                    .map(IntStream::toArray)
-                    .filter(a -> (a[0] >= a[2] && a[0] <= a[3]) ||  (a[1] >= a[2] && a[1] <= a[3]) || (a[0] <= a[2] && a[1] >= a[3]) ||  (a[0] >= a[2] && a[1] <= a[3]))
-                    .count();
+            return getResult(stream, this::overLapFilter);
         }
-
     }
+    private long getResult(Stream<String> stream, Predicate<int[]> filter) {
+        return stream
+                .map(s -> s.replace(",", "-").split("-"))
+                .map(a -> Arrays.stream(a).mapToInt(Integer::parseInt))
+                .map(IntStream::toArray)
+                .filter(filter)
+                .count();
+    }
+
+    private boolean overLapFilter(int[] a) {
+        return (a[0] >= a[2] && a[0] <= a[3])
+                || (a[1] >= a[2] && a[1] <= a[3])
+                || (a[0] <= a[2] && a[1] >= a[3])
+                || (a[0] >= a[2] && a[1] <= a[3]);
+    }
+
+    private boolean subSetFilter(int[] a) {
+        return (a[0] <= a[2] && a[1] >= a[3]) || (a[0] >= a[2] && a[1] <= a[3]);
+    }
+
 }
