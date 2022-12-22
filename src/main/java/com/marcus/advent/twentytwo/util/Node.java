c0d6614811ceb1249f51831882e8e0762995cf72
Marcus Aidley <marcus.aidley@globalrelay.net>
2022-12-22 11:30:45 +0000

    Days 11 and 12 and 13

--- src/main/java/com/marcus/advent/twentytwo/util/Node.java
+++ src/main/java/com/marcus/advent/twentytwo/util/Node.java
@@ -1,2 +1,26 @@
-package com.marcus.advent.twentytwo.util;public class Node {
+package com.marcus.advent.twentytwo.util;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+public class Node<T> {
+    private T value;
+    @Setter private Integer distance = Integer.MAX_VALUE;
+    @Setter private List<Node<T>> shortestPath = new LinkedList<>();
+    private Map<Node<T>, Integer> adjacentNodes = new HashMap<>();
+
+    public Node(T value) {
+        this.value = value;
+    }
+
+    public void addDestination(Node<T> node, int distance) {
+        if (this == node) throw new IllegalArgumentException("Can't connect node to itself");
+        this.adjacentNodes.put(node, distance);
+    }
 }
