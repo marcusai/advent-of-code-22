ebdeb06504920c8d67d2e2183564476ba2b91fb6
Marcus Aidley <marcus.aidley@globalrelay.net>
2022-12-22 11:29:19 +0000

    Days 11 and 12 and 13

--- src/main/java/com/marcus/advent/twentytwo/util/Coord.java
+++ src/main/java/com/marcus/advent/twentytwo/util/Coord.java
@@ -1,2 +1,17 @@
-package com.marcus.advent.twentytwo.util;public class Coord {
+package com.marcus.advent.twentytwo.util;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data @AllArgsConstructor
+public class Coord {
+    public Coord(int x, int y) {
+        this.x = x;
+        this.y = y;
+        value = 0;
+    }
+    public int x;
+    public int y;
+    public int value ;
 }
