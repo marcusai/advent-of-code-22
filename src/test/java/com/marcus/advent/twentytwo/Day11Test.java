01bf4be98568547b95a2b1fb1517420714edb809
Marcus Aidley <marcus.aidley@globalrelay.net>
2022-12-22 11:29:59 +0000

    Days 11 and 12 and 13

--- src/test/java/com/marcus/advent/twentytwo/Day11Test.java
+++ src/test/java/com/marcus/advent/twentytwo/Day11Test.java
@@ -1,6 +1,18 @@
 package com.marcus.advent.twentytwo;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
 class Day11Test {
-  
+    @Test
+    void part1() {
+        assertEquals(78678, new Day11().part1());
+    }
+
+    @Test
+    void part2() {
+        assertEquals(15333249714L, new Day11().part2());
+    }
+
 }
