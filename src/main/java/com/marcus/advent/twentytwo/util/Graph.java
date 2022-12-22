d91556cdd7c5cd13038bd0f2c4e13bede127e11c
Marcus Aidley <marcus.aidley@globalrelay.net>
2022-12-22 11:30:38 +0000

    Days 11 and 12 and 13

--- src/main/java/com/marcus/advent/twentytwo/util/Graph.java
+++ src/main/java/com/marcus/advent/twentytwo/util/Graph.java
@@ -1,2 +1,64 @@
-package com.marcus.advent.twentytwo.util;public class Graph {
+package com.marcus.advent.twentytwo.util;
+
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Set;
+
+import lombok.Getter;
+
+public class Graph<T> {
+    @Getter
+    private Set<Node<T>> nodes = new HashSet<>();
+
+    public void addNode(Node<T> node) {
+        nodes.add(node);
+    }
+
+    public Graph calculateShortestPathFromSource(Graph graph, Node source) {
+        source.setDistance(0);
+
+        Set<Node<T>> settledNodes = new HashSet<>();
+        Set<Node<T>> unsettledNodes = new HashSet<>();
+
+        unsettledNodes.add(source);
+
+        while (unsettledNodes.size() != 0) {
+            Node<T> currentNode = getLowestDistanceNode(unsettledNodes);
+            unsettledNodes.remove(currentNode);
+            for (Map.Entry<Node<T>, Integer> adjacencyPair:
+                    currentNode.getAdjacentNodes().entrySet()) {
+                Node<T> adjacentNode = adjacencyPair.getKey();
+                Integer edgeWeight = adjacencyPair.getValue();
+                if (!settledNodes.contains(adjacentNode)) {
+                    calculateMinimumDistance(adjacentNode, edgeWeight, currentNode);
+                    unsettledNodes.add(adjacentNode);
+                }
+            }
+            settledNodes.add(currentNode);
+        }
+        return graph;
+    }
+    private Node<T> getLowestDistanceNode(Set <Node<T> > unsettledNodes) {
+        Node<T> lowestDistanceNode = null;
+        int lowestDistance = Integer.MAX_VALUE;
+        for (Node<T> node: unsettledNodes) {
+            int nodeDistance = node.getDistance();
+            if (nodeDistance < lowestDistance) {
+                lowestDistance = nodeDistance;
+                lowestDistanceNode = node;
+            }
+        }
+        return lowestDistanceNode;
+    }
+
+    private void calculateMinimumDistance(Node<T> evaluationNode, Integer edgeWeigh, Node sourceNode) {
+        Integer sourceDistance = sourceNode.getDistance();
+        if (sourceDistance + edgeWeigh < evaluationNode.getDistance()) {
+            evaluationNode.setDistance(sourceDistance + edgeWeigh);
+            LinkedList<Node<T>> shortestPath = new LinkedList<>(sourceNode.getShortestPath());
+            shortestPath.add(sourceNode);
+            evaluationNode.setShortestPath(shortestPath);
+        }
+    }
 }
