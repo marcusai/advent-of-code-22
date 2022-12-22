50a21a600602207b42dde51f2cdb0b420e371911
Marcus Aidley <marcus.aidley@globalrelay.net>
2022-12-22 11:30:30 +0000

    Days 11 and 12 and 13

--- src/test/java/com/marcus/advent/twentytwo/Day13Test.java
+++ src/test/java/com/marcus/advent/twentytwo/Day13Test.java
@@ -1,6 +1,21 @@
 package com.marcus.advent.twentytwo;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.IOException;
+
+import org.json.simple.parser.ParseException;
+import org.junit.jupiter.api.Test;
+
 class Day13Test {
-  
+    @Test
+    void part1() throws IOException, ParseException {
+        assertEquals(6623, new Day13().part1());
+    }
+
+    @Test
+    void part2() throws IOException, ParseException {
+        assertEquals(23049, new Day13().part2());
+    }
+
 }
